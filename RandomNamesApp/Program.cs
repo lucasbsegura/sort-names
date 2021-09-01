ea8b9d04fe152181e98869d975b3ec2937908b11
Lucas Baffe Segura <lucas.baffe.segura@accenture.com>
2021-09-01 20:01:42 -0300

    sort names

---
+++ RandomNamesApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SortNamesApp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Sort.Random(Names()).ForEach(name => Console.WriteLine(name));
+
+            Console.ReadKey();
+        }
+
+        public static List<string> Names() => new() { "Arnaldo", "Bruno", "Carlos", "Lucas", "Ronaldo", "Wilson", };
+    }
+
+    public static class Sort
+    {
+        public static List<string> Random(List<string> names) => names.OrderBy(x => Guid.NewGuid()).ToList();
+
+        public static List<string> Alphabetical(List<string> names) => names.OrderBy(i => i).ToList();
+    }
+}
+
